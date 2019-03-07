8e03bd30c612e496584fac085470dd7f3ef057a7
Alberto Marcos Blasco <38374836+albertoomarcos@users.noreply.github.com>
2019-03-07 10:59:07 +0100

    He añadido que los textos ahora sean traducibles
    Creados dos archivos .properties que traduciran los string al idioma de nuestro ordenador

--- WebContent/sapui5_app/controller/App.controller.js
+++ WebContent/sapui5_app/controller/App.controller.js
@@ -1,9 +1,10 @@
 sap.ui.define([ 
 	"sap/ui/core/mvc/Controller", 
 	"sap/m/MessageToast",
-	"sap/ui/model/json/JSONModel" 
+	"sap/ui/model/json/JSONModel",
+	 "sap/ui/model/resource/ResourceModel" 
 	], 
-	function(Controller, MessageToast, JSONModel) {
+	function(Controller, MessageToast, JSONModel, ResourceModel) {
 	"use strict";
 	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
 		onInit : function() {
@@ -15,6 +16,11 @@ sap.ui.define([
 			};
 			var oModel = new JSONModel(oData);
 			this.getView().setModel(oModel);
+			
+			var i18nModel = new ResourceModel({
+	            bundleName: "sapui5_app.i18n.i18n"
+	         });
+	         this.getView().setModel(i18nModel, "i18n");
 		},
 		onShowHello : function() {
 			// show a native JavaScript alert
@@ -22,6 +28,14 @@ sap.ui.define([
 		},
 		onShowHelloToast : function() {
 			MessageToast.show("Hello World with toast");
+		},
+		onShowHelloTraducido : function()
+		{
+	         var oBundle = this.getView().getModel("i18n").getResourceBundle();
+	         var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+	         var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+	         // show message
+	         MessageToast.show(sMsg);
 		}
 	});
 });
