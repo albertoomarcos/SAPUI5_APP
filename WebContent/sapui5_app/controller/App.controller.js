56c99406ff52aff6fc41b281075b80787df54b0b
Alberto Marcos Blasco <38374836+albertoomarcos@users.noreply.github.com>
2019-03-07 10:08:00 +0100

    Añadiendo un mensaje de alerta con un toast
    He creado un botón que cuando lo cliques mostrara una alerta, esta vez con un toast.
     La función se encuentra en el controlador.

--- WebContent/sapui5_app/controller/App.controller.js
+++ WebContent/sapui5_app/controller/App.controller.js
@@ -1,11 +1,16 @@
 sap.ui.define([
-	   "sap/ui/core/mvc/Controller"
-	], function (Controller) {
+	   "sap/ui/core/mvc/Controller",
+	   "sap/m/MessageToast"
+	], function (Controller, MessageToast) {
 	   "use strict";
 	   return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
 	      onShowHello : function () {
 	         // show a native JavaScript alert
 	         alert("Hello World");
+	      },
+	      onShowHelloToast : function()
+	      {
+	    	  MessageToast.show("Hello World with toast");
 	      }
 	   });
 	});
