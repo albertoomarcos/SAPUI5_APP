114d23672ff2f6eec95997bed48570cea838e0c8
Alberto Marcos Blasco <38374836+albertoomarcos@users.noreply.github.com>
2019-03-07 10:15:35 +0100

    Añadiendo un INPUT JSON
    Agregarego un campo de entrada a laaplicación, vincularemos su valor al modelo y vincularemos el mismo valor a la descripción del campo de entrada. La descripción se actualizará directamente según los tipos de usuarios.

--- WebContent/sapui5_app/controller/App.controller.js
+++ WebContent/sapui5_app/controller/App.controller.js
@@ -1,16 +1,27 @@
-sap.ui.define([
-	   "sap/ui/core/mvc/Controller",
-	   "sap/m/MessageToast"
-	], function (Controller, MessageToast) {
-	   "use strict";
-	   return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-	      onShowHello : function () {
-	         // show a native JavaScript alert
-	         alert("Hello World");
-	      },
-	      onShowHelloToast : function()
-	      {
-	    	  MessageToast.show("Hello World with toast");
-	      }
-	   });
+sap.ui.define([ 
+	"sap/ui/core/mvc/Controller", 
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel" 
+	], 
+	function(Controller, MessageToast, JSONModel) {
+	"use strict";
+	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+		onInit : function() {
+			// set data model on view
+			var oData = {
+				recipient : {
+					name : "World"
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.getView().setModel(oModel);
+		},
+		onShowHello : function() {
+			// show a native JavaScript alert
+			alert("Hello World");
+		},
+		onShowHelloToast : function() {
+			MessageToast.show("Hello World with toast");
+		}
 	});
+});
